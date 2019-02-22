8abf1a34e898b96e1dac72c4a91e5ea269496381
xiechanglei <709955748@qq.com>
2019-02-22 14:21:42 +0800

    简单工厂

--- src/me/coldsoul/code/dp/factory/Benz.java
+++ src/me/coldsoul/code/dp/factory/Benz.java
@@ -1,10 +1,12 @@
 package me.coldsoul.code.dp.factory;
 
+import me.coldsoul.code.dp._util.LOGGER;
+
 public class Benz implements Car {
 
 	@Override
 	public void getName() {
-		System.out.println("this is benz");
+		LOGGER.info("this is benz");
 	}
 
 }
--- src/me/coldsoul/code/dp/factory/Bmw.java
+++ src/me/coldsoul/code/dp/factory/Bmw.java
@@ -1,10 +1,12 @@
 package me.coldsoul.code.dp.factory;
 
-public class Bmw implements Car{
+import me.coldsoul.code.dp._util.LOGGER;
+
+public class Bmw implements Car {
 
 	@Override
 	public void getName() {
-		System.out.println("this is bmw");
+		LOGGER.info("this is bmw");
 	}
 
 }
